aafa1a53806167952ccbb4df7128e8abfc90ff94
Ayushver <vermaayu793@gmail.com>
2025-05-12 22:23:26 +0530

    Update AppConstant.java
    updaeted

--- hotelapp/src/main/java/com/hotelapp/constant/AppConstant.java
+++ hotelapp/src/main/java/com/hotelapp/constant/AppConstant.java
@@ -1,6 +1,7 @@
 package com.hotelapp.constant;
 
+
 public class AppConstant {
     public static final String TOPIC = "order_topic";
-    public static final String KAFKA_HOST = "localHost:9092";
+    public static final String KAFKA_HOST = "kafka-broker:9092";
 }
